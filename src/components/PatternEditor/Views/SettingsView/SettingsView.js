836473c2f0f817259a065f061f28c969c5f228a3
Erwin Hoogerwoord <erwinhoogerwoord@erwins-mbp-2.home>
2021-01-08 12:20:31 +0100

    implemented settings view page with possibility of toggling patterns to public or not

--- src/components/PatternEditor/Views/SettingsView/SettingsView.js
+++ src/components/PatternEditor/Views/SettingsView/SettingsView.js
@@ -1,12 +1,59 @@
 import React, { useContext } from 'react'
 import { EditorContext } from '../../PatternEditor'
+import {
+    Input,
+    Form,
+    Message,
+    Checkbox
+} from 'semantic-ui-react'
+import './settings-view.scss'
+import _ from 'lodash'
+import { PatternContext } from 'App'
+
 
 const SettingsView = () => {
     const editor = useContext(EditorContext)
-    console.log(editor)
+    const patternContext = useContext(PatternContext)
     return (
         <div className='settings-view'>
-            <h2>Pattern settings</h2>
+            <div className='ui container'>
+                <div className='centered-content'>
+                <h2>Pattern settings</h2>
+                <Message color='orange' size='tiny'>Work in progress page.</Message>
+                <div className='ui form'>
+                <Form.Field>
+                        <label>Pattern name</label>
+                        <Input
+                            value={editor.pattern.name}
+                            />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Pattern id</label>
+                        <Input
+                            value={editor.pattern._id}
+                            />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Public</label>
+                        <Checkbox
+                            checked={_.get(editor.pattern, 'public', false)}
+                            onChange={() => {
+                                patternContext.actions.updatePattern({
+                                    ...editor.pattern,
+                                    public: !_.get(editor.pattern, 'public', true)
+                                })
+                            }}
+                            />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Iframe embed</label>
+                        <Input
+                            value={`https://pattern.hyperobjects.design/embed/?pattern=${editor.pattern._id}&mode=EDITOR`}
+                            />
+                    </Form.Field>
+                </div>
+                </div>
+            </div>
         </div>
     )
 }
